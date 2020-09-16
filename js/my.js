b102809661d71b61452bce77c52270d9db44724a
Sameer Chauhan <sameer55chauhan@gmail.com>
2020-09-16 16:56:30 +0530

    Transformation:

--- js/my.js
+++ js/my.js
@@ -1,8 +1,8 @@
 function w3_open() {
-  $("#mySidebar").style.width = "100%";
-  $("#mySidebar").style.display = "block";
+  document.getElementById("mySidebar").style.width = "100%";
+  document.getElementById("mySidebar").style.display = "block";
 }
 
 function w3_close() {
-  $("#mySidebar").style.display = "none";
+  document.getElementById("mySidebar").style.display = "none";
 }
