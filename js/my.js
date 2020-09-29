b12c710ac9db85235295889688f464b8b7c548dd
Sameer Chauhan <sameer55chauhan@gmail.com>
2020-09-29 12:11:02 +0530

    design changes

--- js/my.js
+++ js/my.js
@@ -1,12 +1,35 @@
-const guru = gsap.timeline();
-guru.add(ani1());
+const timeline = gsap.timeline();
+timeline.from("#first > img, #first > a",{opacity:0,y:-100, duration:1, ease: "none"});
+timeline.from("#vidpause",{x:-200,duration:0.2, ease: Power3.easeInOut});
+timeline.from(".gfromRight",{x:200, duration:1, stagger:0.1, ease: Power3.easeInOut},"-=0.2");
 
+gsap.from(".g-opacity", 1, {opacity:0 , y:-100, ease: Power3.easeInOut, scrollTrigger: ".g-opacity"});
+gsap.from(".g-fromLeft", 1, {opacity:0, x:-200, ease: Power3.easeInOut, scrollTrigger: ".g-fromLeft"});
+gsap.from(".g-fromRight", 1, {opacity:0, x:200, ease: Power3.easeInOut, scrollTrigger: ".g-fromRight"});
 
-// guru animations
+/*var controller = new ScrollMagic.Controller();
 
-function ani1(){
-	const tl = gsap.timeline();
-	tl.from("#first > img, #first > a",{opacity:0,y:-100, duration:1, ease: "none"});
-	tl.from("#vidpause",{x:-200,duration:0.2, ease: Power3.easeInOut});
-	tl.from(".gfromRight",{x:200, duration:1, stagger:0.1, ease: Power3.easeInOut},"-=0.2");
-}
+new ScrollMagic.Scene({
+	duration: 100;
+	offset: 50;
+}).setPin('#timeline').addTo(controller);*/
+
+$(function()
+{
+    var button = $('.button');
+
+    function switchToNext()
+    {
+        var _this = $(this);
+        
+        if(_this.hasClass('active'))
+            return false;
+        else
+        {
+            $('.button.active').removeClass('active');
+            _this.addClass('active');
+        }
+    }
+
+    button.on('click',switchToNext);
+});
