7e81b7b29970c5b77d4915670b9fa23a694a6d47
Sameer Chauhan <sameer55chauhan@gmail.com>
2020-10-01 09:51:06 +0530

    minor fixes

--- js/my.js
+++ js/my.js
@@ -14,23 +14,23 @@ timeline.from("#first > *",{opacity:0,y:-100, duration:1, ease: Power3.easeInOut
 timeline.from("#vidpause",{x:-200,duration:0.2, ease: Power3.easeInOut});
 timeline.from(".gfromRight",{x:200, duration:1, stagger:0.1, ease: Power3.easeInOut},"-=0.2");
 
-gsap.from("#timeline_1 .g-fromLeft", {scrollTrigger: { trigger: "#timeline_1", start: "0px 80%"},opacity: 0, duration:2, x:-100, ease: "power1.out"});
-gsap.from("#timeline_1 .g-fromRight", {scrollTrigger: { trigger: "#timeline_1", start: "0px 80%"},opacity: 0, duration: 2, x: 100, ease: "power1.out"});
-gsap.from("#timeline_1 .g-opacity", {scrollTrigger: { trigger: "#timeline_1 .g-opacity", start: "0px 90%"},opacity: 0, duration: 2, ease: "power1.out"});
+gsap.from("#timeline_1 .g-fromLeft", {scrollTrigger: { trigger: "#timeline_1", start: "0px 80%"},opacity: 0, duration:1.5, x:-100, ease: "power1.out"});
+gsap.from("#timeline_1 .g-fromRight", {scrollTrigger: { trigger: "#timeline_1", start: "0px 80%"},opacity: 0, duration: 1.5, x: 100, ease: "power1.out"});
+gsap.from("#timeline_1 .g-opacity", {scrollTrigger: { trigger: "#timeline_1 .g-opacity", start: "0px 90%"},opacity: 0, duration: 1.5, ease: "power1.out"});
 
-gsap.from("#timeline_2 .g-fromLeft", {scrollTrigger: { trigger: "#timeline_2", start: "0px 80%"},opacity: 0, duration: 2, x:-100, ease: "power1.out"});
-gsap.from("#timeline_2 .g-fromRight", {scrollTrigger: { trigger: "#timeline_2", start: "0px 80%"},opacity: 0, duration: 2, x: 100, ease: "power1.out"});
-gsap.from("#timeline_2 .g-opacity", {scrollTrigger: { trigger: "#timeline_2", start: "0px 80%"},opacity: 0, duration: 2, ease: "power1.out"});
+gsap.from("#timeline_2 .g-fromLeft", {scrollTrigger: { trigger: "#timeline_2", start: "0px 80%"},opacity: 0, duration: 1.5, x:-100, ease: "power1.out"});
+gsap.from("#timeline_2 .g-fromRight", {scrollTrigger: { trigger: "#timeline_2", start: "0px 80%"},opacity: 0, duration: 1.5, x: 100, ease: "power1.out"});
+gsap.from("#timeline_2 .g-opacity", {scrollTrigger: { trigger: "#timeline_2", start: "0px 80%"},opacity: 0, duration: 1.5, ease: "power1.out"});
 
-gsap.from("#service .g-opacity", {scrollTrigger: {trigger: "#service", start: "0px 80%"}, opacity: 0, duration: 2, ease: "power1.out"});
-gsap.from("#service .g-card", {scrollTrigger: {trigger: "#service", start: "0px 80%"}, opacity: 0, duration: 2, y: 200, ease: "power1.out", stagger: 0.3});
+gsap.from("#service .g-opacity", {scrollTrigger: {trigger: "#service", start: "0px 80%"}, opacity: 0, duration: 1.5, ease: "power1.out"});
+gsap.from("#service .g-card", {scrollTrigger: {trigger: "#service", start: "0px 80%"}, opacity: 0, duration: 1.5, y: 200, ease: "power1.out", stagger: 0.3});
 
-gsap.from("#projectsHead", {scrollTrigger: {trigger: "#projectsHead", start: "0px 80%"}, opacity: 0, duration: 2, ease: "power1.out"});
-gsap.from("#service2 .g-card", {scrollTrigger: {trigger: "#service2", start: "0px 80%"}, opacity: 0, duration: 2, y: 200, ease: "power1.out", stagger: 0.3});
+gsap.from("#projectsHead", {scrollTrigger: {trigger: "#projectsHead", start: "0px 80%"}, opacity: 0, duration: 1.5, ease: "power1.out"});
+gsap.from("#service2 .g-card", {scrollTrigger: {trigger: "#service2", start: "0px 80%"}, opacity: 0, duration: 1.5, y: 200, ease: "power1.out", stagger: 0.3});
 
-gsap.from("#contactMain .g-opacity", {scrollTrigger: {trigger: "#contactMain", start: "0px 80%"}, opacity: 0, duration: 2, ease: "power1.out"});
-gsap.from("#contactSecond .g-fromLeft", {scrollTrigger: { trigger: "#contactSecond", start: "0px 80%"},opacity: 0, duration: 2, x:-100, ease: "power1.out"});
-gsap.from("#contactSecond .g-fromRight", {scrollTrigger: { trigger: "#contactSecond", start: "0px 80%"},opacity: 0, duration: 2, x: 100, ease: "power1.out"});
+gsap.from("#contactMain .g-opacity", {scrollTrigger: {trigger: "#contactMain", start: "0px 80%"}, opacity: 0, duration: 1.5, ease: "power1.out"});
+gsap.from("#contactSecond .g-fromLeft", {scrollTrigger: { trigger: "#contactSecond", start: "0px 80%"},opacity: 0, duration: 1.5, x:-100, ease: "power1.out"});
+gsap.from("#contactSecond .g-fromRight", {scrollTrigger: { trigger: "#contactSecond", start: "0px 80%"},opacity: 0, duration: 1.5, x: 100, ease: "power1.out"});
 
 
 $(function()
